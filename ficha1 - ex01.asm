9b85ac1754c025984e1b34f625ee193b0f59c98d
iza <126572391+izabellealencar05@users.noreply.github.com>
2023-10-11 15:40:49 -0300

    Create ficha1 - ex01.asm

---
+++ ficha1 - ex01.asm
@@ -0,0 +1,7 @@
+;Faça um programa que atribua aos registradores AL, BL, CL e DL, os valores FA; FB; FC e FD
+respectivamente.
+    MOV    AL, FA
+    MOV    BL, FB
+    MOV    CL, FC
+    MOV    DL, FD
+    END
