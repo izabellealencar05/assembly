758e4eec5158cacbcccedc7845c2c21ef5cff328
iza <126572391+izabellealencar05@users.noreply.github.com>
2023-10-18 15:07:05 -0300

    Update ficha1 - ex04.asm

--- ficha1 - ex04.asm
+++ ficha1 - ex04.asm
@@ -1,11 +1,11 @@
 ;Faça um programa que apresente no display de sete segmentos (porta 02) o valor 88;
 
-  MOV  AL,00
-  OUT  02
-  INC  AL
-  OUT  02
-  NOT  AL
-  OUT  02
-  INC  AL
-  OUT  02
+  MOV  AL,00   ;00 esta sendo movido para o Reg AL;
+  OUT  02      ;envia o valor de AL para a porta de E/S 02;
+  INC  AL      ;incrementa o valor de AL +1, ent agr AL vale 01;
+  OUT  02      ;nvm envia o novo valor de AL para a porta de E/S 02;
+  NOT  AL      ;negou bit por bit do valor, 00000001 ficou 11111110;
+  OUT  02      ;o novo valor de AL foi enviando para a porta de E/S 02;
+  INC  AL      ;incrementa +1 no valor de AL, logo o valor agr eh 11111111;
+  OUT  02      ;envia o novo valor de AL para a porta de E/S 02;
   END
