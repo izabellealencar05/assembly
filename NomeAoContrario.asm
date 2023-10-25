b244fc10a2b22fd0fefbf35d0e214a59dbb72a1b
iza <126572391+izabellealencar05@users.noreply.github.com>
2023-10-25 13:36:12 -0300

    Update NomeAoContrario.asm

--- NomeAoContrario.asm
+++ NomeAoContrario.asm
@@ -1,19 +1,19 @@
-	      ORG 0
-	JMP  	INICIO
-	ORG	  10
-	DB	"izabelle alencar"
-	ORG	  20	  ;define inicio	
+	ORG 0
+	JMP  		INICIO
+	ORG		10
+	DB		"izabelle alencar"
+	ORG		20	  	
 inicio:
-	MOV	    CL,C0
-	MOV	    DL,10
-	MOV	    BL,1F
+	MOV	    	CL,C0
+	MOV	    	DL,10
+	MOV	    	BL,1F
 							
 PROX:	
-	MOV  	  AL,[BL]
-	MOV	    [CL],AL
-	INC	    CL		
-	INC	    BL
-	DEC  	  DL
-	JNZ	    PROX
+	MOV  	  	AL,[BL]
+	MOV	  	[CL],AL
+	INC	  	CL		
+	INC	  	DL
+	DEC  	 	BL
+	JNZ	  	PROX
 	END
 
