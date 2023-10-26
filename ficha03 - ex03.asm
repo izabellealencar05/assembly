10b3b6430b9b3a59e5886e9b70c528fc405f7fb9
iza <126572391+izabellealencar05@users.noreply.github.com>
2023-10-26 14:41:58 -0300

    Update ficha03 - ex03.asm

--- ficha03 - ex03.asm
+++ ficha03 - ex03.asm
@@ -1,7 +1,7 @@
 	mov	cl,60	; cl recebe valor para endereco inicial
 	mov	al,05	; al recebe o valor inicial
-	mov	dl,10	; dl recebe o valor do contador. s�o 16 elementos
-soma:	mov	[cl],al	; valor de al ser� armazenado no endere�o indicado em cl
+	mov	dl,10	; dl recebe o valor do contador. sao 16 elementos
+soma:	mov	[cl],al	; valor de al ser armazenado no endereco indicado em cl
 	add	al,02	
 	inc	cl	; aponta para a proxima posicao de memoria
 	dec	dl	;decrementa o contador
@@ -9,11 +9,11 @@ soma:	mov	[cl],al	; valor de al ser� armazenado no endere�o indicado em cl
 	
 	mov	al,[60]	; al recebe primeiro valor da memoria
 	mov	cl,61	; vou pegar a aprtir do segundo elemento na memmoria
-	mov	dl,0f	; contador agora s�o s� 15 elementos
+	mov	dl,0f	; contador agora sao so 15 elementos
 xorr:	mov	bl,[cl]	;leio proximo valor
-	xor	al,bl	;fa�o xor entreles, al ir� acumular o resultado
+	xor	al,bl	;faz o xor entreles, al ir acumular o resultado
 	inc	cl	; cl aponta para proximo elemento
 	dec	dl	; decremento contador
-	jnz	xorr	; dl chegou a zero? sen�o faz proximo xor
+	jnz	xorr	; dl chegou a zero? senao faz proximo xor
 	mov	[70],al	; se terminou salva resultado do xor
 	end
